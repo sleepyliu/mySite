03705bc8440848786a5b8ee23af553201c579cc2
sleepyliu <lingjie26@126.com>
2015-03-07 06:42:32 +0000

    finish bootstrap

--- mySite/settings.py
+++ mySite/settings.py
@@ -87,9 +87,9 @@ STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "static"),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 
 SITE_ID = 1
